905d234d05fd3b58e54f28a38a7c45c7e6c205ba
Nikita K <nikwhite333@gmail.com>
2024-07-02 02:05:59 +0200

    First Load command parsing - WIP

--- macho_parser.py
+++ macho_parser.py
@@ -3,12 +3,15 @@ import os
 
 MH_MAGIC_64 = "feedfacf"
 
-MH_EXECUTE = "02000000"
-MH_DYLIB = "06000000"
+MH_EXECUTE = "02"
+MH_DYLIB = "06"
+
+LC_SEGMENT = "01"
+LC_SEGMENT_64 = "19"
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("binary")
+    parser.add_argument("binary", help="Mach object binary file path (executable/.dylib)")
     args = parser.parse_args()
 
     binary_path = args.binary
@@ -30,16 +33,21 @@ def main():
         cpusubtype = file_handle.read(4).hex()
         print(cpusubtype)
 
-        filetype = file_handle.read(4).hex()
+        filetype = file_handle.read(4).hex().rstrip("0")
         print("File type: ", filetype)
         if filetype != MH_DYLIB and filetype != MH_EXECUTE:
             raise Exception("Unknown file type")
 
-        ncmds = file_handle.read(4).hex()
-        print(ncmds)
+        ncmds_bytes = file_handle.read(4)
+        print("ncmds bytes", ncmds_bytes)
+        ncmds_str = ncmds_bytes.hex().rstrip("0")
+        print("ncmds hex str", ncmds_str)
+        #ncmds_int = int(f"0x{ncmds_str}", 0)
+        ncmds_int = int(ncmds_str, 16)
+        print("ncmds (decimal): ", ncmds_int)
 
-        sizeofcmds = file_handle.read(4).hex()
-        print(sizeofcmds)
+        sizeofcmds = file_handle.read(4).hex().rstrip("0")
+        print("sizeofcmds: ", sizeofcmds)
 
         flags = file_handle.read(4).hex()
         print(flags)
@@ -47,6 +55,13 @@ def main():
         reserved = file_handle.read(4).hex()
         print(reserved)
 
+        # Load commands
+        cmd = file_handle.read(4).hex().rstrip("0")
+        cmdsize = file_handle.read(4).hex().rstrip("0")
+        segname = file_handle.read(16).decode().rstrip("\x00")
+        print(cmd, cmdsize, segname)
+        if cmd != LC_SEGMENT_64:
+            raise Exception()
 
 if __name__ == "__main__":
     main()
